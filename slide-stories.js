74b8e89a003afee93764d20257c87c84ae606298
Rafael Prado <rafael.prado@mandarin.com.br>
2020-09-04 10:21:15 -0300

    update popup

---
+++ slide-stories.js
@@ -0,0 +1,52 @@
+const popup = document.querySelector(`[data-slide=slide]`);
+
+function modal(index) {
+  popup.classList.remove("hide");
+
+  new SlideStories("slide", index);
+}
+
+class SlideStories {
+  constructor(id, position) {
+    this.slide = document.querySelector(`[data-slide="${id}"]`);
+    this.active = 0;
+    this.init(position);
+  }
+
+  activeSlide(index) {
+    this.active = index;
+    this.items.forEach((item) => item.classList.remove("active"));
+    this.items[index].classList.add("active");
+  }
+
+  prev() {
+    if (this.active > 0) {
+      this.activeSlide(this.active - 1);
+    } else {
+      this.activeSlide(this.items.length - 1);
+    }
+  }
+
+  next() {
+    if (this.active < this.items.length - 1) {
+      this.activeSlide(this.active + 1);
+    } else {
+      this.activeSlide(0);
+    }
+  }
+
+  addNavigation() {
+    const nextBtn = this.slide.querySelector(".slide-next");
+    const prevBtn = this.slide.querySelector(".slide-prev");
+    nextBtn.addEventListener("click", this.next);
+    prevBtn.addEventListener("click", this.prev);
+  }
+
+  init(position) {
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.items = this.slide.querySelectorAll(".slide-items > *");
+    this.activeSlide(position);
+    this.addNavigation();
+  }
+}
